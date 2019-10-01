0bd4c3130d3f6c6d1cae56af49aa4b4899f76a6c
talvane-lima <talvane.glauber@gmail.com>
2019-10-01 13:43:19 -0300

    Function to insert doc in mongo and hash url

--- App/connect_mongo.php
+++ App/connect_mongo.php
@@ -5,5 +5,5 @@ $pass = "2z4cHUttV9>w";
 $host = "mongo";
 $port = "27017";
 
-$mongo = new Mongo('mongodb://'.$user.':'.$pass.'@'.$host.':'.$port);
+$connection = new Mongo('mongodb://'.$user.':'.$pass.'@'.$host.':'.$port);
 ?>
--- App/mmore_api.php
+++ App/mmore_api.php
@@ -1,8 +1,25 @@
 <?php
-require_once 'connect_mongo.php';
 
-$db = $mongo->short_url_db;
-$col = $db->urls;
-$total = $col->count();
-echo $total;
+function insert_mongo($user_id, $url)
+{	
+	include 'connect_mongo.php';
+	$short_url_db = $connection->selectDB('short_url_db');
+	$urls = $short_url_db->selectCollection('urls');
+
+	$base_new_url = 'https://mmore/';
+	$new_url = hash('Adler32', $url);
+
+	$insert_data = array(
+		'user_id' => $user_id,
+		'original_url' => $url,
+		'short_url' => $base_new_url.$new_url,
+		'saved_at' => new MongoDate(),
+		'number_clicks' => 0
+	);
+
+    $result = $urls->insert($insert_data);
+    return $result;
+}
+
+echo var_dump(insert_mongo("mm001", "https://www.uol.com"));
 ?>
