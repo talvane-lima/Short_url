b80794ac36585a0f4346d7c480d8661a192bcc5d
talvane-lima <talvane.glauber@gmail.com>
2019-10-01 14:10:17 -0300

    Function to find short url in mongoDB

--- App/mmore_api.php
+++ App/mmore_api.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+	Insert a new document in mongoDB
+	$user_id: User ID
+	$urls: Original url to create a tiny url
+
+	return: The new document inserted on mongoDB
+*/
 function insert_mongo($user_id, $url)
 {	
 	include 'connect_mongo.php';
@@ -17,9 +24,34 @@ function insert_mongo($user_id, $url)
 		'number_clicks' => 0
 	);
 
-    $result = $urls->insert($insert_data);
+    $urls->insert($insert_data);
+
+    return $insert_data;
+}
+
+
+/*
+	Find document ($short_url) on mongoDB
+	$short_url: Short url to find on mongoDB
+
+	return: The document content $short_url
+*/
+function find_url($short_url)
+{	
+	include 'connect_mongo.php';
+	$short_url_db = $connection->selectDB('short_url_db');
+	$urls = $short_url_db->selectCollection('urls');
+
+	$find_data = array(
+		'short_url' => $short_url
+	);
+
+	$result = $urls->findOne($find_data);
+	$urls->update($find_data, array('$set' => array('number_clicks' => $result['number_clicks']+1)));
+	$result = $urls->findOne($find_data);
+
     return $result;
 }
 
-echo var_dump(insert_mongo("mm001", "https://www.uol.com"));
+echo json_encode(find_url("https://mmore/479e071c"));
 ?>
